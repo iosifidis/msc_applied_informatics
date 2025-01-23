61fd22457e0577b92a1ea5a4bd8ef4cbd2e45af1
Efstathios Iosifidis <iefstathios@gmail.com>
2025-01-23 19:47:17 +0200

    some changes on the code

--- s1/Programming - Software Engineering in Business/excersices/week9/src-v1/Main.java
+++ s1/Programming - Software Engineering in Business/excersices/week9/src-v1/Main.java
@@ -3,15 +3,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//Φτιάχνω αντικείμενο πλοίο με 450 κοντέινερ (ΕΡΩΤΗΜΑ 
+		//Φτιάχνω αντικείμενο πλοίο με 450 κοντέινερ (ΕΡΩΤΗΜΑ Β)
 		Ship ship1 = new Ship("Zeus", 450);
 
-		//Φτιάχνω αντικείμενα πλοία		
+		//Φτιάχνω αντικείμενα container		
 		Bulk container1 = new Bulk("XY101", "Barcelona", 980);
 		Refridgerator container2 = new Refridgerator("XX250", "Piraeus", 11.7);
 		Bulk container3 = new Bulk("AB430", "Genova", 1700);
 		Refridgerator container4 = new Refridgerator("KL330", "Chania", 5.8);
 		
+		// Καταχωρώ τα containers στο πλοίο
 		ship1.addContainer(container1);
 		ship1.addContainer(container2);
 		ship1.addContainer(container3);
