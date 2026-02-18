915838e690d4d401abc4a4b51f019a4a1db686a6
ckopsahilis <ckopsahilis@gmail.com>
2026-02-18 22:33:06 +0200

    Adding exams 2026

---
+++ s1/Software Systems Engineering/exams26/thema1/src/exams/SelectionSort.java
@@ -0,0 +1,26 @@
+package exams;
+
+public class SelectionSort implements SortAlgorithm {
+
+	public SelectionSort() {}
+
+	@Override
+	public void sort(Object[] array) {
+		//SelectionSort algorithm for sorting students
+		 int i, j, first;
+		 Student temp;  
+	     for ( i = array.length - 1; i > 0; i -- )  
+	     {
+	          first = 0;   //initialize to subscript of first element
+	          for(j = 1; j <= i; j ++)   //locate smallest element between positions 1 and i.
+	          {
+	               if( ((Student) array[j]).compareTo((Student) array[first]) > 0 )         
+	                 first = j;
+	          }
+	          temp = (Student)array[ first ];   //swap smallest found with element in position i.
+	          array[ first ] = array[ i ];
+	          array[ i ] = temp; 
+	      }        
+	}
+
+}
---
+++ s1/Software Systems Engineering/exams26/thema1/src/exams/StudentList.java
@@ -0,0 +1,31 @@
+package exams;
+
+import java.util.ArrayList;
+
+public class StudentList {
+    
+    private ArrayList<Student> list;
+
+    public StudentList() {
+        list = new ArrayList<Student>();
+        
+        // Default data moved here
+        this.add(new Student("Maria", "Papadopoulou", 6.78));
+        this.add(new Student("Giorgos", "Nikolaou", 8.41));
+        this.add(new Student("Eleni", "Nikolaidou", 8.88));
+        this.add(new Student("Petros", "Asteriou", 7.34));
+    }
+
+    public void add(Student s) {
+        list.add(s);
+    }
+
+    public ArrayList<Student> getList() {
+        return list;
+    }
+    
+    // Helper to clear list (useful when re-populating after sort)
+    public void clear() {
+        list.clear();
+    }
+}
